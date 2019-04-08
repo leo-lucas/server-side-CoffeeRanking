570a8a275b3c5c25e42f1fe8f111576621bfc175
Leonardo Lucas dos Santos <leolucassang@gmail.com>
2019-04-08 00:21:21 -0300

    criando arquivos

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,12 @@
 // node-typescript/src/server.ts
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 const server = express();
 
 server.get("/", (_, res) => {
+  // readline para le arquivos
+
   res.send("Hello ts-node!");
 });
 
